0fb96ba35ffc2a6ed52a258fae50d0fcb1f94b5b
dabirax <oliverpraise1@gmail.com>
2025-12-19 04:15:54 +0100

    build: fix logo import

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion"
 import { Link } from "@tanstack/react-router"
 
-const Logo = () => {
+export default function Logo() {
   return (
       <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.85 }} className="italic font-bold text-[#9017c2] text-2xl">
           <Link to="/">FV</Link>
@@ -9,4 +9,3 @@ const Logo = () => {
   )
 }
 
-export default Logo
